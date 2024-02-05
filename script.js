c394b03803b2503f534ce2398033988dba0c3596
realwally1 <il.yaallen2008@yahoo.com.au>
2024-02-05 18:05:59 +1300

    changes

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const socialButton = document.querySelector('#social_button')
 const socialSection = document.querySelector('.socials')
 const winnersButton = document.querySelector('#winners_button')
 const buttonsHeader = document.querySelector('.button_intro')
+const homeButton = document.querySelector('.home_button')
 
 socialButton.addEventListener('click', function() {
     socialSection.style.transform = 'translateY(-200px)'
     winnersButton.style.transform = 'translateX(1500px)'
     socialButton.style.transform = 'translateX(-1500px)'
     buttonsHeader.style.transform = 'translateX(1500px)'
+})
+
+homeButton.addEventListener('click', function() {
+    socialSection.style.transform = 'translateY(1500px)'
+    winnersButton.style.transform = 'translateX(0px)'
+    socialButton.style.transform = 'translateX(0px)'
+    buttonsHeader.style.transform = 'translateX(0px)'
 })
